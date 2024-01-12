bd7c5e9eba85993d5498827df39113a66cddc1f7
Joshua Phillips <joshua.phillips@brivo.com>
2024-01-11 19:20:23 -0500

    updating the slides to be formatted correctly

--- docs/convert_to_dz.sh
+++ docs/convert_to_dz.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Convert Markdown to DZ Slides
-pandoc XYZ_Strategic_Plan_Slides.md -t dzslides -o XYZ_Strategic_Plan_Slides.html
+pandoc XYZ_Strategic_Plan_Slides.md --standalone --to dzslides -o XYZ_Strategic_Plan_Slides.html
